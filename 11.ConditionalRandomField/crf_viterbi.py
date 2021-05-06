838e31c70ca8cb0a587ee887ff89f8e3441d1a0f
HuangStomach <nxmbest@qq.com>
2021-05-06 21:01:23 +0800

    转移至维特比算法

--- 11.ConditionalRandomField/crf_viterbi.py
+++ 11.ConditionalRandomField/crf_viterbi.py
@@ -3,7 +3,7 @@ import numpy as np
 from copy import deepcopy
 from typing import TypeVar
 
-# 条件随机场模型学习的改进的迭代尺度算法
+# 条件随机场模型学习的维特比算法
 P = TypeVar('P', bound='CRFIIS')
 class CRFIIS:
     def __init__(self, times = 1, epsilon = 0.001) -> None:
